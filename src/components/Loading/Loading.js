184e575c425a40a8f8183394f11fe7232ff4d82e
Anastasiia_Zinovieva <avzinovieva62@gmail.com>
2020-09-15 15:24:12 +0300

    loading fixed

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -4,8 +4,7 @@ import t from  '../../translations/i18n'
 import {Redirect} from 'react-router-dom';
 import {USER_TOKEN} from '../../storageKeys';
 
-
-class Loading extends Component {
+/*class Loading extends Component {
     constructor(props) {
         super(props);
 
@@ -13,47 +12,25 @@ class Loading extends Component {
             isLogged : false,
         };
     }
-    componentWillMount=async ()=>{
-        this.state.isLogged = (!!(await localStorage.getItem(USER_TOKEN)));
-        if (this.state.isLogged){
-            return (
-                <Redirect to="/orderList"/>
-            )
-        }
-        return (
-            <Redirect to="/login"/>
-        )
-
-    }
-
-    onLoad = async () => {
-       this.state.isLogged = (!!(await localStorage.getItem(USER_TOKEN)));
-       this.state.isLogged ? <Redirect to="/orderList"/> : <Redirect to="/login"/>;
-    }
 
     render() {
         return (
             <div>
                 <h1>{`${t('login.header')}`}</h1>
                 <Corgi/>
-                {/*{this.state.isLogged ? <Redirect to="/orderList"/> : <Redirect to="/login"/>}*/}
-                {/*{this.state.isLogged ? alert("/orderList") : alert("/login")}*/}
+                {/!*{this.state.isLogged ? <Redirect to="/orderList"/> : <Redirect to="/login"/>}*!/}
+                {/!*{this.state.isLogged ? alert("/orderList") : alert("/login")}*!/}
             </div>
         )
     };
-}
-/*
+}*/
 const Loading = () =>{
     return(
         <div>
-
-            {console.log(onLoad())}
-            { onLoad().then((v)=> v ?  : )}
+            {localStorage.getItem(USER_TOKEN) ? <Redirect to="/orderList"/> : <Redirect to="/login"/>}
         </div>
-
     );
 
 }
-*/
 
 export  default  Loading;
