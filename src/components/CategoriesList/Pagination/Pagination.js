ef7eb41767662fc2007f0daa49665f2d1bd407c4
Anastasiia_Zinovieva <avzinovieva62@gmail.com>
2020-10-04 19:33:57 +0300

    git comments fixed

--- src/components/CategoriesList/Pagination/Pagination.js
+++ src/components/CategoriesList/Pagination/Pagination.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
