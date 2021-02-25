f3ec226f27dd51ef68f57aaee1b5ae1ae4b013a8
Admin <kostian.mendes@gmail.com>
2021-02-25 10:37:27 +0200

    Create new branch

---
+++ src/components/WorkTypesDetails/WorkTypesDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopBar from '../TopBar/TopBar';
+
+const WorkTypesDetails = () => {
+    return (
+        <div>
+            <TopBar />
+
+            test
+        </div>
+    );
+};
+
+export default WorkTypesDetails;
