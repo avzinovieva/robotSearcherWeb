2878d790631fefa33b1655569f27572122a62fa6
Anastasiia <avzinovyeva62@gmail.com>
2021-03-02 14:11:50 +0200

    added config file

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,22 +4,22 @@ import NavBar from './NavBar/NavBar';
 import RightBar from './RightBar/RightBar';
 
 import styles from './topBar.module.scss';
+import version from '../../configure';
 
-const TopBar = () => {
-  return (
-    <div className={styles.navigation}>
-      <div className={styles.wrapper}>
-        <img
-          src={logo}
-          alt='logo'
-          className={styles.logo}
-          onClick={() => window.location.reload()}/>
-          <p className={styles.version}>1.0.0</p>
-        <NavBar />
-        <RightBar/>
-      </div>
+const TopBar = () => (
+  <div className={styles.navigation}>
+    <div className={styles.wrapper}>
+      <img
+        src={logo}
+        alt="logo"
+        className={styles.logo}
+        onClick={() => window.location.reload()}
+      />
+      <p className={styles.version}>{version}</p>
+      <NavBar />
+      <RightBar />
     </div>
-  );
-};
+  </div>
+);
 
 export default TopBar;
