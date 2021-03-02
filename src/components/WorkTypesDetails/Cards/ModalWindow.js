130c434690283dfb58d068b77a31212a164f915d
Admin <kostian.mendes@gmail.com>
2021-03-02 22:52:06 +0200

    Add modal window for

---
+++ src/components/WorkTypesDetails/Cards/ModalWindow.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../WorkTypesDetails.module.scss'
+
+const AddNewWorkTypeCard = () => {
+    return (
+        <div>
+            <div className={styles.title}>Work types details</div>
+          {console.log('work')}
+        </div>
+    );
+};
+export default AddNewWorkTypeCard;
