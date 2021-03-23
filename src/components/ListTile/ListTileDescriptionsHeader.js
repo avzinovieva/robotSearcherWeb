fb084fa9cd7e81fec6484dd216b43391a008a8f5
Admin <kostian.mendes@gmail.com>
2021-03-23 15:01:20 +0200

    Fixed landing pages and header padding of table header

--- src/components/ListTile/ListTileDescriptionsHeader.js
+++ src/components/ListTile/ListTileDescriptionsHeader.js
@@ -3,12 +3,12 @@ import t from '../../translations/i18n';
 import styles from './listTile.module.scss';
 
 const ListTileDescriptionsHeader = () => (
-    <div className={styles.tileWrapperHeaderDescriptions}>
-        <div className={styles.p__name}>{`${t('ordersList.orderTableHeaders.userName')}`}</div>
-        <div className={styles.p__orderId}>{`${t('ordersList.orderTableHeaders.orderId')}`}</div>
-        <div className={styles.p__requestData}>{`${t('ordersList.orderTableHeaders.requestData')}`}</div>
-        <div className={styles.p__moneyAmount}>{`${t('ordersList.orderTableHeaders.moneyAmount')}`}</div>
-    </div>
+  <div className={styles.tileWrapperHeaderDescriptions}>
+    <div className={styles.p__name}>{`${t('ordersList.orderTableHeaders.userName')}`}</div>
+    <div className={styles.p__orderId}>{`${t('ordersList.orderTableHeaders.orderId')}`}</div>
+    <div className={styles.p__requestData}>{`${t('ordersList.orderTableHeaders.requestData')}`}</div>
+    <div className={styles.p__moneyAmount}>{`${t('ordersList.orderTableHeaders.moneyAmount')}`}</div>
+  </div>
 );
 
 export default ListTileDescriptionsHeader;
